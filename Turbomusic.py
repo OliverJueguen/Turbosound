b086ca56cbb184c8139b0d2a1daec3fc0e9102f6
Oliver Jv <45468588+OliverJueguen@users.noreply.github.com>
2023-03-17 22:13:13 +0100

    Updated with a clean code

--- Turbomusic.py
+++ Turbomusic.py
@@ -1,54 +1,103 @@
 import discord
 from discord.ext import commands
-from discord.voice_client import VoiceClient
-import youtube_dl
-import asyncio
+import yt_dlp as youtube_dl
 
-bot = commands.Bot(command_prefix='!', description='Este es un bot de música.')
-bot_token = 'TOKEN_DEL_BOT'
+# Suppress noise about console usage from errors
+youtube_dl.utils.bug_reports_message = lambda: ''
 
+# Set up the bot prefix and description
+bot_prefix = "!"
+bot_description = "Este es un bot de música."
+
+# Set up the intents
+intents = discord.Intents.all()
+
+# Create the bot instance with the set prefix, description, and intents
+bot = commands.Bot(command_prefix=bot_prefix, description=bot_description, intents=intents)
+
+
+# Check if the bot is ready
+@bot.event
+async def on_ready():
+    print(f"{bot.user.name} está listo para la música!")
+
+
+# Join the voice channel of the user who summoned the bot
 @bot.command()
 async def join(ctx):
-    channel = ctx.message.author.voice.channel
+    channel = ctx.author.voice.channel
     await channel.connect()
 
-@bot.command()
-async def play(ctx, url):
-    server = ctx.message.guild
-    voice_channel = server.voice_client
 
-    try:
-        async with ctx.typing():
-            player = await YTDLSource.from_url(url, loop=bot.loop)
-            voice_channel.play(player, after=lambda e: print('Error: %s' % e) if e else None)
-        await ctx.send('Reproduciendo: {}'.format(player.title))
-
-    except:
-        await ctx.send('Error: No se puede reproducir la canción.')
-
+# Leave the voice channel of the bot
 @bot.command()
 async def leave(ctx):
-    voice_client = ctx.message.guild.voice_client
-    await voice_client.disconnect()
+    await ctx.voice_client.disconnect()
 
-class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.5):
-        super().__init__(source, volume)
 
-        self.data = data
+# Play the requested song
+@bot.command()
+async def play(ctx, *, query):
+    voice_client = ctx.voice_client or await ctx.author.voice.channel.connect()
 
-        self.title = data.get('title')
-        self.url = ""
+    with youtube_dl.YoutubeDL({
+        'outtmpl': 'song.%(ext)s',
+        'format': 'bestaudio/best',
+        'quiet': True,
+        'nocheckcertificate': True,
+        'source_address': '0.0.0.0',
+        'no-call-home': True
+    }) as ydl:
+        info = ydl.extract_info(f"ytsearch:{query}", download=False)['entries'][0]
+        url = info['url']
+        title = info['title']
 
-    @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
-        loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: youtube_dl.YoutubeDL({'format': 'bestaudio/best', 'quiet': True}).extract_info(url, download=False))
-        if 'entries' in data:
-            data = data['entries'][0]
-        filename = data['title'] if stream else youtube_dl.utils.sanitize_filename(data['title'], restricted=True)
-        url = data['url']
-        return cls(discord.FFmpegPCMAudio(url, **ffmpeg_options), data=data)
+        voice_client.stop()
+        source = await discord.FFmpegOpusAudio.from_probe(url, method='fallback')
+        voice_client.play(source)
 
-if __name__ == '__main__':
-    bot.run(bot_token)
+        await ctx.send(f"Reproduciendo: {title}")
+
+
+# Pause the current song
+@bot.command()
+async def pause(ctx):
+    voice_client = ctx.voice_client
+    if voice_client and voice_client.is_playing():
+        voice_client.pause()
+        await ctx.send("Pausado.")
+
+
+# Resume the current song
+@bot.command()
+async def resume(ctx):
+    voice_client = ctx.voice_client
+    if voice_client and voice_client.is_paused():
+        voice_client.resume()
+        await ctx.send("Reanudado.")
+
+
+# Stop playing the current song
+@bot.command()
+async def stop(ctx):
+    voice_client = ctx.voice_client
+    if voice_client and voice_client.is_playing():
+        voice_client.stop()
+        await ctx.send("Detenido.")
+
+
+# Set up error handling
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandInvokeError):
+        original = error.original
+        if isinstance(original, discord.ClientException):
+            await ctx.send("Ya estoy reproduciendo una canción.")
+        elif isinstance(original, youtube_dl.utils.ExtractorError):
+            await ctx.send("No se puede reproducir la canción.")
+    else:
+        raise error
+
+
+# Run the bot
+bot.run("TOKEN")
